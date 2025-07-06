aada340eece2b0ae20c11b19efa7de65ea381092
Michael Fotiadis <mikemike19991@gmail.com>
2025-07-06 22:18:56 +0300

    Created main-content component.

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { Header } from './header/header';
 import { Footer } from "./footer/footer";
 import { RouterOutlet } from '@angular/router';
+import { MainContent } from './main-content/main-content';
 
 @Component({
   selector: 'app-root',
-  imports: [Header, Footer,RouterOutlet],
+  imports: [RouterOutlet,Header, Footer,MainContent],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
