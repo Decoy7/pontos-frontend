2974aa360ce85ab0f4f3e54309611521f55fac71
Michael Fotiadis <mikemike19991@gmail.com>
2025-07-06 22:05:38 +0300

    Added stuff.

--- src/app/footer/footer.ts
+++ src/app/footer/footer.ts
@@ -1,12 +1,17 @@
 import { Component } from '@angular/core';
+import { LucideAngularModule, Github  } from 'lucide-angular';
 
 @Component({
-  standalone: true,
   selector: 'app-footer',
-  imports: [],
+  standalone: true,
+  imports: [LucideAngularModule],
   templateUrl: './footer.html',
-  styleUrl: './footer.css'
+  styleUrls: ['./footer.css']
 })
 export class Footer {
+  currentYear = new Date().getFullYear();
 
+  icons = {
+    Github,
+  };
 }
--- src/app/header/header.ts
+++ src/app/header/header.ts
@@ -1,12 +1,18 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   standalone: true,
   selector: 'app-header',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './header.html',
   styleUrl: './header.css'
 })
 export class Header {
+  isMobileMenuOpen = false;
+
+  toggleMobileMenu() {
+    this.isMobileMenuOpen = !this.isMobileMenuOpen;
+  }
 
 }
